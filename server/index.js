4d8c97a2c9111841440b021b8c03ea5cca0c3691
João André Lopes Carias <carias.joao@gmail.com>
2019-07-26 01:08:10 +0100

    Changed the tempalate to be a real HTML document and not a constant string.
    Signed-off-by: João André Lopes Carias <carias.joao@gmail.com>

--- server/index.js
+++ server/index.js
@@ -2,21 +2,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const pdf = require("html-pdf");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const pdfTemplate = require("./documents");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+const RESULT_FILE_NAME = "result.pdf";
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const template = fs.readFileSync(
+  path.resolve(__dirname, "./documents/template.html"),
+  "utf8"
+);
+
 //POST PDF generation and fetch the data
 app.post("/create-pdf", (req, res) => {
   console.log(req.body);
-  pdf.create(pdfTemplate(req.body), {}).toFile("result.pdf", err => {
+
+  var templateWithData = template.replace(
+    "{ TEMPLATE_DATA }",
+    JSON.stringify(req.body)
+  );
+
+  pdf.create(templateWithData, {}).toFile(RESULT_FILE_NAME, err => {
     if (err) {
       res.send(Promise.reject());
     }
@@ -27,7 +41,7 @@ app.post("/create-pdf", (req, res) => {
 
 //GET - Send the generated PDF to the client
 app.get("/fetch-pdf", (req, res) => {
-  res.sendFile(`${__dirname}/result.pdf`);
+  res.sendFile(`${__dirname}/${RESULT_FILE_NAME}`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
